9fe3dc4963c002940fc894aa9af0d61b3e7226e6
PhanQuocKhanh29 <88721369+PhanQuocKhanh29@users.noreply.github.com>
2021-09-07 08:11:40 +0700

    Add project files.

---
+++ WebApplication1/Controllers/GiaiPTController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class GiaiPTController : Controller
+    {
+        // GET: GiaiPT
+        public ActionResult GiaiPT()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GiaiPT(string SoA, string SoB)
+        {
+            double so1 = Convert.ToDouble(SoA);
+            double so2 = Convert.ToDouble(SoB);
+            double ketqua = -so2 / so1;
+            ViewBag.GPT = ketqua;
+            return View();
+        }
+    }
+}
